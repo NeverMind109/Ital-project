b4bf3c2f8c94905e4eb10a14faab225ccf77e920
NeverMind109 <vladgladkih123@gmail.com>
2022-04-20 18:37:40 +0300

    Created: Added Hero Section + Main Page Header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
 $(function () {
-    
-})
+  $(".hero__slider").slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+
+  $(function () {
+    $(".menu__btn").on("click", function () {
+      $(".menu-list").toggleClass("menu-list--active");
+      $("body").toggleClass("overflow--hidden");
+    });
+    $(".menu-list__link").on("click", function () {
+      $(".menu-list").removeClass("menu-list--active");
+      $("body").removeClass("overflow--hidden");
+    });
+  });
+});
